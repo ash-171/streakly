ea4ba65b334ae710e193ed9dc086e202d5858c91
Aswathi <80941531+ash-171@users.noreply.github.com>
2026-07-07 17:06:57 +0530

    Add Firebase configuration to firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+window.firebaseConfig = {
+  apiKey: "AIzaSyDySLNIRiUKZNPfyidEO55_uIieVyjG-u8",
+  authDomain: "streakly-9859d.firebaseapp.com",
+  databaseURL: "https://streakly-9859d-default-rtdb.firebaseio.com",
+  projectId: "streakly-9859d",
+  storageBucket: "streakly-9859d.firebasestorage.app",
+  messagingSenderId: "915099138308",
+  appId: "1:915099138308:web:417d9af7aac0bcfb107851",
+  measurementId: "G-B6HWSZHZKH"
+};
